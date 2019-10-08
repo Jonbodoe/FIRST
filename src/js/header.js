235e8af2e0a6cd31fe1e127d9ec980f9322ebeae
Jonbodoe <le0220@mail.philau.edu>
2019-10-07 23:07:51 -0400

    media query changes

--- src/js/header.js
+++ src/js/header.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $(window).scroll(function (e) {
-        $('nav').toggleClass('navbar-light bg-light shadow', $(this).scrollTop() > 2100);
-        $('nav').toggleClass('navbar-dark text-white', $(this).scrollTop() < 2100);
+        $('nav').toggleClass('navbar-light bg-light shadow', $(this).scrollTop() > 1500);
+        $('nav').toggleClass('navbar-dark text-white', $(this).scrollTop() < 1500);
     })
   });
