e3d5cef56c8f6cd5fea2ce736ef33af8b96ff461
Jonbodoe <le0220@mail.philau.edu>
2019-10-28 21:49:59 -0400

    homepage redesigned

---
+++ src/js/background.js
@@ -0,0 +1,6 @@
+// $(document).ready(function () {
+//     $(window).scroll(function (e) {
+//         $('#content').toggleClass('headerFire', $(this).scrollTop() > 1200);
+//         $('#content').toggleClass('headerTrees', $(this).scrollTop() < 1200);
+//     })
+// });
--- src/js/header.js
+++ src/js/header.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $(window).scroll(function (e) {
-        $('nav').toggleClass('navbar-light bg-light shadow', $(this).scrollTop() > 1500);
-        $('nav').toggleClass('navbar-dark text-white', $(this).scrollTop() < 1500);
+        $('nav').toggleClass('navbar-light bg-light shadow', $(this).scrollTop() > 1250);
+        $('nav').toggleClass('navbar-dark text-white', $(this).scrollTop() < 1250);
     })
   });
