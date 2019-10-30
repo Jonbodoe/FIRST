de2c364cdc891b11078bae781ff0614caabed7e7
Jonbodoe <le0220@mail.philau.edu>
2019-10-30 18:23:14 -0400

    js added

--- src/js/background.js
+++ src/js/background.js
@@ -1,6 +1,8 @@
-// $(document).ready(function () {
-//     $(window).scroll(function (e) {
-//         $('#content').toggleClass('headerFire', $(this).scrollTop() > 1200);
-//         $('#content').toggleClass('headerTrees', $(this).scrollTop() < 1200);
-//     })
-// });
+$(document).ready(function () {
+    $(window).scroll(function (e) {
+        // $('img#arrow').addClass('hidden', $(this).scrollTop() > 1200);
+        // $('img#arrow').removeClass('hidden', $(this).scrollTop() < 1200)
+        $(this).scrollTop() > 380 ?
+            $('img#arrow').addClass('hidden') : $('img#arrow').removeClass('hidden')
+    })
+});
